a764c85c5933159929ced4e218a73656cd332110
Leon <xiaoyankeji@hotmail.com>
2016-08-07 19:22:56 +0800

    第一次提交

---
+++ 回收站RecycleBin/BitmapModule.vb
@@ -0,0 +1,117 @@
+﻿Imports System.Drawing.Drawing2D
+Imports System.Runtime.InteropServices
+
+Module BitmapModule
+    Private Const ULW_COLORKEY As Int32 = &H1
+    Public Const ULW_ALPHA As Int32 = &H2
+    Private Const ULW_OPAQUE As Int32 = &H4
+    Public Const WS_EX_LAYERED As Int32 = &H80000
+    Public Const AC_SRC_OVER As Byte = &H0
+    Public Const AC_SRC_ALPHA As Byte = &H1
+
+    <StructLayout(LayoutKind.Sequential)>
+    Public Structure mSize
+        Private cx As Int32
+        Private cy As Int32
+        Public Sub New(ByVal cx As Int32, ByVal cy As Int32)
+            Me.cx = cx
+            Me.cy = cy
+        End Sub
+    End Structure
+
+    <StructLayout(LayoutKind.Sequential)>
+    Public Structure mPoint
+        Private x As Int32
+        Private y As Int32
+        Public Sub New(ByVal x As Int32, ByVal y As Int32)
+            Me.x = x
+            Me.y = y
+        End Sub
+    End Structure
+
+    <StructLayout(LayoutKind.Sequential, Pack:=1)>
+    Public Structure BLENDFUNCTION
+        Public BlendOp As Byte
+        Public BlendFlags As Byte
+        Public SourceConstantAlpha As Byte
+        Public AlphaFormat As Byte
+    End Structure
+
+    Public Declare Auto Function GetDC Lib "user32.dll" (ByVal hWnd As IntPtr) As IntPtr
+    Public Declare Auto Function CreateCompatibleDC Lib "gdi32.dll" (ByVal hDC As IntPtr) As IntPtr
+    Public Declare Auto Function SelectObject Lib "gdi32.dll" (ByVal hDC As IntPtr, ByVal hObject As IntPtr) As IntPtr
+    Public Declare Auto Function UpdateLayeredWindow Lib "user32.dll" (ByVal hwnd As IntPtr, ByVal hdcDst As IntPtr, ByRef pptDst As mPoint, ByRef psize As mSize, ByVal hdcSrc As IntPtr, ByRef pprSrc As mPoint, ByVal crKey As Int32, ByRef pblend As BLENDFUNCTION, ByVal dwFlags As Int32) As Boolean
+    Public Declare Auto Function ReleaseDC Lib "user32.dll" (ByVal hWnd As IntPtr, ByVal hDC As IntPtr) As Integer
+    Public Declare Auto Function DeleteObject Lib "gdi32.dll" (ByVal hObject As IntPtr) As Boolean
+    Public Declare Auto Function DeleteDC Lib "gdi32.dll" (ByVal hdc As IntPtr) As Boolean
+
+    Dim hDC1 As IntPtr = GetDC(IntPtr.Zero)
+    Dim hDC2 As IntPtr = CreateCompatibleDC(hDC1)
+    Dim hBitmap1 As IntPtr = IntPtr.Zero
+    Dim hBitmap2 As IntPtr = IntPtr.Zero
+    Dim blend As New BLENDFUNCTION()
+
+    Dim GetBitmap As Bitmap
+    Dim ReturnBitmap As Bitmap
+    Dim MyGraphics As Graphics
+
+    ' 使用Alpha通道把Bitmap绘制到Window
+    Public Sub SetAlphaPicture(ByVal AlphaWindow As Form, ByVal AlphaPicture As Bitmap)
+        Try
+            hBitmap1 = AlphaPicture.GetHbitmap(Color.FromArgb(0))
+            hBitmap2 = SelectObject(hDC2, hBitmap1)
+
+            With blend
+                .BlendOp = AC_SRC_OVER
+                .BlendFlags = 0
+                .AlphaFormat = AC_SRC_ALPHA
+                .SourceConstantAlpha = True
+            End With
+            Call UpdateLayeredWindow(AlphaWindow.Handle, hDC1, New mPoint(AlphaWindow.Left, AlphaWindow.Top), New mSize(AlphaPicture.Width, AlphaPicture.Height), hDC2, New mPoint(0, 0), 0, blend, ULW_ALPHA)
+        Finally
+            Call SelectObject(hDC2, hBitmap2)
+            Call DeleteObject(hBitmap1)
+        End Try
+    End Sub
+
+    '旋转Bitmap图像
+    Public Function GetRotateBitmap(ByVal BitmapRes As Bitmap, ByVal Angle As Single) As Bitmap
+        GetBitmap = BitmapRes
+        If Not ReturnBitmap Is Nothing Then ReturnBitmap.Dispose()
+        ReturnBitmap = New Bitmap(GetBitmap.Width, GetBitmap.Height)
+        MyGraphics = Graphics.FromImage(ReturnBitmap)
+        MyGraphics.TranslateTransform(GetBitmap.Width / 2, GetBitmap.Height / 2)
+        MyGraphics.RotateTransform(Angle, MatrixOrder.Prepend)
+        MyGraphics.TranslateTransform(-GetBitmap.Width / 2, -GetBitmap.Height / 2)
+        MyGraphics.DrawImage(GetBitmap, 0, 0, GetBitmap.Width, GetBitmap.Height)
+        If Not RecycleBinForm.FileIcon Is Nothing Then
+            MyGraphics.DrawImage(RecycleBinForm.FileIcon, 112, 112, 32, 32)
+            MyGraphics.DrawString(RecycleBinForm.FileName, RecycleBinForm.Font, Brushes.White, 112, 150)
+        End If
+        MyGraphics.Dispose()
+        Return ReturnBitmap
+    End Function
+
+    Private Structure SHFILEINFO
+        Public hIcon As IntPtr
+        Public iIcon As Integer
+        Public dwAttributes As Integer
+        <MarshalAs(UnmanagedType.ByValTStr, SizeConst:=260)>
+        Public szDisplayName As String
+        <MarshalAs(UnmanagedType.ByValTStr, SizeConst:=80)>
+        Public szTypeName As String
+    End Structure
+
+    Private Declare Auto Function SHGetFileInfo Lib "shell32.dll" (ByVal pszPath As String, ByVal dwFileAttributes As Integer, ByRef psfi As SHFILEINFO, ByVal cbFileInfo As Integer, ByVal uFlags As Integer) As IntPtr
+    Private Const SHGFI_ICON = &H100
+    Private Const SHGFI_LARGEICON = &H0
+
+    Public Function GetFileIcon(ByVal FilePath As String) As Bitmap
+        Dim SHInfo As New SHFILEINFO()
+        SHInfo.szDisplayName = New String(Chr(0), 260)
+        SHInfo.szTypeName = New String(Chr(0), 80)
+        SHGetFileInfo(FilePath, 0, SHInfo, Marshal.SizeOf(SHInfo), SHGFI_ICON Or SHGFI_LARGEICON)
+        Dim FileIcon As Bitmap = System.Drawing.Icon.FromHandle(SHInfo.hIcon).ToBitmap
+        Return FileIcon
+    End Function
+End Module
---
+++ 回收站RecycleBin/RecycleBinForm.vb
@@ -0,0 +1,124 @@
+﻿Imports System.IO
+Imports Microsoft.VisualBasic.FileIO
+
+Public Class RecycleBinForm
+    Private Declare Function SetParent Lib "user32" Alias "SetParent" (ByVal hWndChild As Integer, ByVal hWndNewParent As Integer) As Integer
+    Private Declare Function FindWindow Lib "user32" Alias "FindWindowA" (ByVal lpClassName As String, ByVal lpWindowName As String) As Integer
+    Private Declare Function FindWindowEx Lib "user32" Alias "FindWindowExA" (ByVal hWnd1 As Integer, ByVal hWnd2 As Integer, ByVal lpsz1 As String, ByVal lpsz2 As String) As Integer
+    Private Declare Function GetDesktopWindow Lib "user32" Alias "GetDesktopWindow" () As Integer
+    Private Declare Function ReleaseCapture Lib "user32" () As Integer
+    Private Declare Function SendMessageA Lib "user32" (ByVal hwnd As Integer, ByVal wMsg As Integer, ByVal wParam As Integer, lParam As VariantType) As Integer
+
+    ''' <summary>
+    ''' 清空回收站
+    ''' </summary>
+    ''' <param name="hwnd">父级句柄</param>
+    ''' <param name="pszRootPath">为空则清空所有驱动器的回收站</param>
+    ''' <param name="dwFlags">参数标识</param>
+    Private Declare Sub SHEmptyRecycleBin Lib "shell32.dll" Alias "SHEmptyRecycleBinA" (ByVal hwnd As IntPtr, ByVal pszRootPath As String, ByVal dwFlags As Integer)
+    Private Const SHERB_NOCONFIRMATION = &H1 '不显示确认提示框
+    Private Const SHERB_NOPROGRESSUI = &H2 '不显示任务进度条
+    Private Const SHERB_NOSOUND = &H4 '不播放清空提示音
+    Public FileIcon As Bitmap, FileName As String
+
+    ''' <summary>
+    ''' 更新回收站图标
+    ''' </summary>
+    Private Declare Function SHUpdateRecycleBinIcon Lib "shell32.dll" () As Integer
+    'API操作成功返回值
+    Const S_OK = &H0
+
+    '黑洞每次旋转的度数
+    Private Const dAngle As Integer = 20
+    '黑洞下一次的旋转角度
+    Dim Angle As Integer = dAngle
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        BitmapModule.SetAlphaPicture(Me, BitmapModule.GetRotateBitmap(My.Resources.RecycleBinRes.BlackHole, Angle))
+        Angle = IIf(Angle = 360, dAngle, Angle + dAngle)
+    End Sub
+    Protected Overloads Overrides ReadOnly Property CreateParams() As CreateParams
+        Get
+            If Not DesignMode Then
+                Dim cp As CreateParams = MyBase.CreateParams
+                cp.ExStyle = cp.ExStyle Or BitmapModule.WS_EX_LAYERED
+                Return cp
+            Else
+                Return MyBase.CreateParams
+            End If
+        End Get
+    End Property
+
+    Private Sub RecycleBinForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        '允许拖拽
+        Me.AllowDrop = True
+        SetParent(Me.Handle, GetDesktopIconHandle)
+        BitmapModule.SetAlphaPicture(Me, My.Resources.RecycleBinRes.BlackHole)
+        Me.Location = New Point(My.Computer.Screen.Bounds.Width - Me.Width, 10)
+    End Sub
+
+    Private Sub RecycleBinForm_DragDrop(sender As Object, e As DragEventArgs) Handles Me.DragDrop
+        Dim FilePaths() As String = e.Data.GetData(DataFormats.FileDrop)
+        For Each File As String In FilePaths
+            Try
+                FileIcon = GetFileIcon(File)
+                FileName = FileSystem.GetName(File)
+                If FileSystem.FileExists(File) Then
+                    FileSystem.DeleteFile(File, UIOption.AllDialogs, RecycleOption.SendToRecycleBin)
+                ElseIf FileSystem.DirectoryExists(File) Then
+                    FileSystem.DeleteDirectory(File, UIOption.AllDialogs, RecycleOption.SendToRecycleBin)
+                End If
+            Finally
+                '执行完毕
+            End Try
+        Next
+        FileIcon = Nothing
+        '停止旋转
+        Timer1.Stop()
+    End Sub
+
+    Public Function GetDesktopIconHandle() As IntPtr
+        '获取物理系统桌面图标的句柄，用于嵌入实现置后显示
+        Dim HandleDesktop As Integer = GetDesktopWindow
+        Dim HandleTop As Integer = 0
+        Dim LastHandleTop As Integer = 0
+        Dim HandleSHELLDLL_DefView As Integer = 0
+        Dim HandleSysListView32 As Integer = 0
+        '在WorkerW结构里搜索
+        Do Until HandleSysListView32 > 0
+            HandleTop = FindWindowEx(HandleDesktop, LastHandleTop, "WorkerW", vbNullString)
+            HandleSHELLDLL_DefView = FindWindowEx(HandleTop, 0, "SHELLDLL_DefView", vbNullString)
+            If HandleSHELLDLL_DefView > 0 Then HandleSysListView32 = FindWindowEx(HandleSHELLDLL_DefView, 0, "SysListView32", "FolderView")
+            LastHandleTop = HandleTop
+            If LastHandleTop = 0 Then Exit Do
+        Loop
+        '如果找到了，立即返回
+        If HandleSysListView32 > 0 Then Return HandleSysListView32
+        '未找到，则在Progman里搜索(用于兼容WinXP系统)
+        Do Until HandleSysListView32 > 0
+            HandleTop = FindWindowEx(HandleDesktop, LastHandleTop, "Progman", "Program Manager")
+            HandleSHELLDLL_DefView = FindWindowEx(HandleTop, 0, "SHELLDLL_DefView", vbNullString)
+            If HandleSHELLDLL_DefView > 0 Then HandleSysListView32 = FindWindowEx(HandleSHELLDLL_DefView, 0, "SysListView32", "FolderView")
+            LastHandleTop = HandleTop
+            If LastHandleTop = 0 Then Exit Do : Return 0
+        Loop
+        Return HandleSysListView32
+    End Function
+
+    Private Sub RecycleBinForm_DragEnter(sender As Object, e As DragEventArgs) Handles Me.DragEnter
+        If (e.Data.GetDataPresent(DataFormats.FileDrop) = True) Then e.Effect = DragDropEffects.All : Timer1.Start()
+    End Sub
+
+    Private Sub RecycleBinForm_MouseUp(sender As Object, e As MouseEventArgs) Handles Me.MouseUp
+        If e.Button = MouseButtons.Right Then End
+    End Sub
+
+    Private Sub RecycleBinForm_DragLeave(sender As Object, e As EventArgs) Handles Me.DragLeave
+        Timer1.Stop()
+    End Sub
+
+    Private Sub RecycleBinForm_MouseDown(sender As Object, e As MouseEventArgs) Handles Me.MouseDown
+        ReleaseCapture()
+        SendMessageA(Me.Handle, &HA1, 2, 0&)
+    End Sub
+End Class
